25645d805b6cb017f39b8cb48860d27240f71d0d
aizel carrera <carrerajg.674.stud@cdd.edu.ph>
2024-07-18 13:48:42 +0800

    add index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-
-
-
-mamamamamaa mooooo
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>docs</title>
+</head>
+<body>
+<h1>John aizel Carrera</h1>
+<h2>block 3</h2>
+<h3>3rd year summer</h3>
+</body>
+</html>
